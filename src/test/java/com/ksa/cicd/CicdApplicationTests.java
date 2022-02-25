90ad591bff1f7944efa935344913352947a436c4
rivera00255 <rivera00255@gmail.com>
2022-02-25 10:11:14 +0900

    FIRST commit

---
+++ src/test/java/com/ksa/cicd/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksa.cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
